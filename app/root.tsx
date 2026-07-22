aa3509abf532b8743021d71e59f4fe89d1ece790
Dima852 <diploma.admn.hotel@gmail.com>
2026-07-22 18:06:37 +0300

    test

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        {/* {children} */}
         <ScrollRestoration />
         <Scripts />
       </body>
